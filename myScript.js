c748223af882785a2c4a37cab30a52466fd008e1
Abdulrahman Jama <abdirahman.jama010@gmail.com>
2024-06-10 21:08:13 +0300

    Add pseudocode as context for every step
    someone might not understand the thought process behind the code

--- myScript.js
+++ myScript.js
@@ -1,13 +1,18 @@
+// - create function called getComputerChoice
 function getComputerChoice() {
+// - declare variable called "random" and assign to it a random number from 0-2 *
 let random = Math.floor(Math.random() * 3);
 
+// - make a conditional that prints "rock" if random is equal to zero
 if (random === 0) {
    console.log("Rock");
 } else if (random === 1) {
+// and prints "paper" if random equal to one
     console.log("Paper");
 } else {
+// and prints "scissors" if it's anything else (which is two)
     console.log("Scissors");
 }
 }
 
-getComputerChoice();
+
