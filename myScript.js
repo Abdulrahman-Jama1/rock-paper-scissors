ab5efe0818939fc99e28b6eb93f70bf38e495ad2
Abdulrahman Jama <abdirahman.jama010@gmail.com>
2024-06-11 21:46:10 +0300

    Revise both get.. to return values not print them
    if i'm to use those two functions as values for another function, they need to return a value first

--- myScript.js
+++ myScript.js
@@ -2,16 +2,15 @@
 function getComputerChoice() {
 // - declare variable called "random" and assign to it a random number from 0-2 *
 let random = Math.floor(Math.random() * 3);
-
 // - make a conditional that prints "rock" if random is equal to zero
 if (random === 0) {
-   console.log("Rock");
+   return "rock";
 } else if (random === 1) {
 // and prints "paper" if random equal to one
-    console.log("Paper");
+    return "paper";
 } else {
 // and prints "scissors" if it's anything else (which is two)
-    console.log("Scissors");
+    return "scissors";
 }
 }
 
@@ -25,8 +24,34 @@ function getHumanChoice() {
     let choice = prompt("what do you choose, will it be rock or paper? maybe scissors?").toLowerCase();
 
     // - print the user input in the console
-    console.log(choice);
+    return choice;
 }
+
+let humanScore = 0;
+let computerScore = 0;
+
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === "rock" && computerChoice === "scissors") {
+        console.log("you win! bravo")
+    }
+}
+
+
+playRound(getHumanChoice(), getComputerChoice());
+
+
+
+
+
+
+
+
+// Create a new function named playRound.
+// Define two parameters for playRound: humanChoice and computerChoice. Use these two parameters to take the human and computer choices as arguments.
+// Make your function’s humanChoice parameter case-insensitive so that players can input “rock”, “ROCK”, “RocK”, or other variations.
+// Write the code for your playRound function to console.log a string value representing the round winner, such as: “You lose! Paper beats Rock”.
+// Increment the humanScore or computerScore variable based on the round winner.
 
 
 
